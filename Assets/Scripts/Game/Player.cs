947c1f9cfd929c2036d72583ab4d8f7ddb6a44c6
Thymustw <Thymusjp1@gmail.com>
2023-12-30 03:50:15 +0900

    Update JsonReader, delete rod class, change test player.cs

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,45 +1,52 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Player : MonoBehaviour
 {
     private LineRenderer lineRenderer;
 
-    IEnumerator fishing;
+    IEnumerator putDownTheRope;
 
-    Vector3 currentPos;
+    Vector3 ropeCurrentPos;
 
-    float rotAngle = 0.1f;
+    float rodPlusAngle = 0.1f;
+
+    float rodSpeed;
 
     bool goBack, nextFishing, startRot;
 
-    // Start is called before the first frame update
+
     void Awake()
     {
         lineRenderer = transform.GetChild(0).GetComponent<LineRenderer>();
 
-        lineRenderer.SetPosition(0, Vector3.zero);
-        lineRenderer.SetPosition(1, Vector3.zero);
+        SetLineRendererPosition(Vector3.zero);
 
-        fishing = PutDownTheRope();
+        putDownTheRope = PutDownTheRope();
+        rodSpeed = JsonReader.Instance.GetRodSpeed("NormalFishingRod");
 
-        nextFishing = true;
-        startRot = true;
+        SetFishing(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetFish();
+        // Rotate the rod.
         RotateRod();
 
+        // Rope max length detact.
         GoBackDetact(-1280f);
 
+        // Fishing detact.
         if (Input.GetButtonDown("Fire1") && nextFishing)
         {
-            StartCoroutine(fishing);
-            nextFishing = false;
-            startRot = false;
+            StartCoroutine(putDownTheRope);
+
+            SetLineRendererPosition(new Vector3(0, -100, -1));
+
+            SetFishing(false);
         }
     }
 
@@ -48,49 +55,86 @@ public class Player : MonoBehaviour
         if(startRot)
         {   
             if (transform.GetChild(0).rotation.z >= 0.3f)
-            {
-                rotAngle*=-1;
-            }
+                rodPlusAngle*=-1;
             else if (transform.GetChild(0).rotation.z <= -0.3f)
-            {
-                rotAngle*=-1;
-            }
+                rodPlusAngle*=-1;
 
-            transform.GetChild(0).Rotate(transform.rotation.x, transform.rotation.y, rotAngle);
+            transform.GetChild(0).Rotate(transform.rotation.x, transform.rotation.y, rodPlusAngle);
         }
     }
 
     void GoBackDetact(float endPos)
     {
-        if((currentPos.y <= endPos || Mathf.Sin(Mathf.Deg2Rad * transform.GetChild(0).rotation.z * 180 / 1.56f) * currentPos.y >= 540 || Mathf.Sin(Mathf.Deg2Rad * transform.GetChild(0).rotation.z * 180 / 1.56f) * currentPos.y <= -540) && !goBack)
+        float ropeLength = Mathf.Sin(Mathf.Deg2Rad * transform.GetChild(0).rotation.z * 180 / 1.56f) * ropeCurrentPos.y;
+        
+        // Detact if the rope out the scene.
+        if((ropeCurrentPos.y <= endPos || ropeLength >= 540 || ropeLength <= -540) && !goBack)
         {
+            GetFish();
             goBack = true;
         }
-        else if (currentPos.y >= 0 && goBack)
+        // If the rope is go back.
+        else if (ropeCurrentPos.y >= -100 && goBack)
         {
-            StopCoroutine(fishing);
+            StopCoroutine(putDownTheRope);
+
+            SetLineRendererPosition(Vector3.zero);
 
             goBack = false;
 
-            nextFishing = true;
-            startRot = true;
+            SetFishing(true);
         }
     }
 
     IEnumerator PutDownTheRope()
     {
-        Vector3 startPos = new Vector3(0,0,0);
+        Vector3 startPos = new Vector3(0, -100, -1);
+
         while(true)
         {
             yield return new WaitForFixedUpdate();
 
             if(goBack)
-                currentPos = startPos += new Vector3(0, 200f * Time.fixedDeltaTime, 0);
+                ropeCurrentPos = startPos += new Vector3(0, 200f * Time.fixedDeltaTime * rodSpeed, 0);
             else
-                currentPos = startPos -= new Vector3(0, 200f * Time.fixedDeltaTime, 0);
+            {
+                ropeCurrentPos = startPos -= new Vector3(0, 200f * Time.fixedDeltaTime * rodSpeed, 0);
+            }
 
-
-            lineRenderer.SetPosition(1, currentPos);
+            lineRenderer.SetPosition(1, ropeCurrentPos);
         }
     }
+
+    void GetFish()
+    {
+        Debug.DrawRay(transform.GetChild(0).position, -transform.GetChild(0).up, Color.green);
+        //RaycastHit2D hit;
+        if(Physics2D.Raycast(ropeCurrentPos, Vector2.down, 1, 1 << 5))
+            Debug.Log("HitUI");
+    }
+
+    //!!!---Important---!!!
+
+    // Change the Rod's func.
+    public void ChangeSpeed(string rodName)
+    {
+        rodSpeed = JsonReader.Instance.GetRodSpeed(rodName);
+    }
+
+    //!!!---Important---!!!
+
+    #region "Tools"
+    void SetLineRendererPosition(Vector3 vector3)
+    {
+        lineRenderer.SetPosition(0, vector3);
+        lineRenderer.SetPosition(1, vector3);
+    }
+
+    void SetFishing(bool value)
+    {
+        nextFishing = value;
+        startRot = value;
+    }
+    #endregion
+
 }
--- Assets/Scripts/Tools/JsonReader.cs
+++ Assets/Scripts/Tools/JsonReader.cs
@@ -1,5 +1,36 @@
 using UnityEngine;
 using System.IO;
+using System.Collections.Generic;
+
+#region "Json"
+[System.Serializable]
+class RodsData
+{
+    public List<Rod> Rod;
+}
+
+[System.Serializable]
+class Rod
+{
+    public string Name;
+    public int RopeDownSpeed;
+    public bool IsOwn;
+}
+
+[System.Serializable]
+class FishsData
+{
+    public FishOwnsNum[] FishOwnsNum;
+}
+
+[System.Serializable]
+class FishOwnsNum
+{
+    public string Name;
+    public int Price;
+    public int Num;
+}
+#endregion
 
 public class JsonReader
 {
@@ -14,8 +45,144 @@ public class JsonReader
         }
     }
 
-    public string JsonText(string jsonName)
+    string GetJsonText(string jsonName)
     {
         return File.ReadAllText(Application.dataPath + "/Data/" + jsonName);
     }
+
+
+    #region "Rod"
+    // Get if the target rod is Own.
+    public bool GetRodIsOwn(string rodName)
+    {
+        string jsonData = GetJsonText("FishingRodTypes.json");
+        RodsData root = JsonUtility.FromJson<RodsData>(jsonData);
+
+        bool isOwn = false;
+        foreach (Rod rod in root.Rod)
+        {
+            if(rod.Name == rodName)
+                isOwn = rod.IsOwn;
+        }
+        return isOwn;
+    }
+
+    public void SetRodIsOwn(string rodName, bool rodIsOwn)
+    {
+        string jsonData = GetJsonText("FishingRodTypes.json");
+        RodsData root = JsonUtility.FromJson<RodsData>(jsonData);
+
+        foreach (Rod rod in root.Rod)
+        {
+            if(rod.Name == rodName)
+                rod.IsOwn = rodIsOwn;
+        }
+
+        string json = JsonUtility.ToJson(root, true);
+        
+        using (StreamWriter sr = new StreamWriter(Application.dataPath + "/Data/FishingRodTypes.json"))
+        {
+            sr.WriteLine(json);
+            sr.Close();
+        }
+    }
+
+    // Get the target rod's speed.
+    public float GetRodSpeed(string rodName)
+    {
+        string jsonData = GetJsonText("FishingRodTypes.json");
+        RodsData root = JsonUtility.FromJson<RodsData>(jsonData);
+
+        float temp = 0;
+        foreach (Rod rod in root.Rod)
+        {
+            if(rod.Name == rodName)
+                temp = rod.RopeDownSpeed;
+        }
+        return temp;
+    }
+
+    // For gacha.
+    public List<string> GetAllRod()
+    {
+        string jsonData = GetJsonText("FishingRodTypes.json");
+        RodsData root = JsonUtility.FromJson<RodsData>(jsonData);
+
+        List<string> tempList = new List<string>();
+        foreach (Rod rod in root.Rod)
+        {
+            tempList.Add(rod.Name);
+        }
+
+        return tempList;
+    }
+    #endregion
+
+
+    #region "FishBag"
+    // Get the target fish's price.
+    public int GetFishPrice(string fishName)
+    {
+        string jsonData = GetJsonText("FishBag.json");
+        FishsData root = JsonUtility.FromJson<FishsData>(jsonData);
+
+        int temp = 0;
+        foreach (FishOwnsNum fish in root.FishOwnsNum)
+        {
+            if(fish.Name == fishName)
+                temp = fish.Price;
+        }
+        return temp;
+    }
+
+    // Get the target fish's own number.
+    public int GetFishNum(string fishName)
+    {
+        string jsonData = GetJsonText("FishBag.json");
+        FishsData root = JsonUtility.FromJson<FishsData>(jsonData);
+
+        int temp = 0;
+        foreach (FishOwnsNum fish in root.FishOwnsNum)
+        {
+            if(fish.Name == fishName)
+                temp = fish.Num;
+        }
+        return temp;
+    }
+
+    public void SetFishNum(string fishName, int fishNum)
+    {
+        string jsonData = GetJsonText("FishBag.json");
+        FishsData root = JsonUtility.FromJson<FishsData>(jsonData);
+
+        foreach (FishOwnsNum fish in root.FishOwnsNum)
+        {
+            if(fish.Name == fishName)
+                fish.Num = fishNum;
+        }
+
+        string json = JsonUtility.ToJson(root, true);
+        
+        using (StreamWriter sr = new StreamWriter(Application.dataPath + "/Data/FishBag.json"))
+        {
+            sr.WriteLine(json);
+            sr.Close();
+        }
+    }
+
+    // For Fish generate.
+    public List<string> GetAllFish()
+    {
+        string jsonData = GetJsonText("FishBag.json");
+        FishsData root = JsonUtility.FromJson<FishsData>(jsonData);
+
+        List<string> tempList = new List<string>();
+        foreach (FishOwnsNum fish in root.FishOwnsNum)
+        {
+            tempList.Add(fish.Name);
+        }
+
+        return tempList;
+    }
+    #endregion
 }
